9e381475c7444f7f1a1749dc2635de513d6417f4
Sankalp12DevOps <sankalp12m@gmail.com>
2023-03-14 21:36:22 +0530

    sds

--- components/mysqldb.sh
+++ components/mysqldb.sh
@@ -8,18 +8,18 @@ source components/common.sh
 enableStartService
 TEMP_PASSWORD=$(grep "temporary password" /var/log/mysqld.log | awk -F : '{print $NF}')
 
-echo "show databases;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE
+echo "show databases;" | mysql -uroot -pRoboShop@1   &>>$LOG_FILE
 if [ $? -ne 0 ]; then
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Roboshop@1';" | mysql --connect-expired-password -uroot -p${TEMP_PASSWORD} &>> ${LOG_FILE}
 exitcode $? "Password Changed"
 fi
 echo "show plugins;" | mysql -uroot -pRoboShop@1 | grep validate_password &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
-"uninstall plugin validate_password;"| mysql  -uroot -pRoboShop@1 &>> $LOG_FILE
+"uninstall plugin validate_password;"| mysql  -uroot -pRoboShop@1 &>>$LOG_FILE
 exitcode $? "Uninstalled validate_password plugin"
 fi
 
-curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip" &>> ${LOG_FILE}
+curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip" &>>${LOG_FILE}
 exitcode $? "Downloading Shipping Repo"
 cd /tmp
 unzip mysql.zip &>> ${LOG_FILE}
