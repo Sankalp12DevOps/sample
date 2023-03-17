ee83ff4569b55317546f7ccbaf6a5a450f49845b
Sankalp12DevOps <sankalp12m@gmail.com>
2023-03-17 17:31:05 +0530

    hello

--- create-ec2.sh
+++ create-ec2.sh
@@ -8,13 +8,13 @@ HOSTEDZONEID=Z01817288AYQJ8IWO87B
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SECURITY_GROUPID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=robosecurity | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 COMPONENT=$1
-PRIVATEIP=echo $(aws ec2 run-instances \
+PRIVATEIP=echo "$(aws ec2 run-instances \
     --image-id $AMI_ID \
     --count 1 \
     --instance-type t2.micro \
     --security-group-ids $SECURITY_GROUPID \
     --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].NetworkInterfaces[].PrivateIpAddress' | sed -e 's/"//g')
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq '.Instances[].NetworkInterfaces[].PrivateIpAddress' | sed -e 's/"//g')"
 
 sed -i "s/COMPONENT/$COMPONENT" -i "s/IPADDRESS/$PRIVATEIP" components/record.json > /tmp/r53.json
 
