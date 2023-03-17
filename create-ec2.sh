7ec4c7a2f3101fe3771ec3e839703212c94fdabd
Sankalp12DevOps <sankalp12m@gmail.com>
2023-03-17 08:15:53 +0530

    hello

--- create-ec2.sh
+++ create-ec2.sh
@@ -7,13 +7,18 @@ fi
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos7-Linux-Learning-AMI" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SECURITY_GROUPID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=robosecurity | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
-# $(aws ec2 run-instances \
-#     --image-id $AMI_ID \
-#     --count 1 \
-#     --instance-type t2.micro \
-#     --security-group-ids $SECURITY_GROUPID \
-#     --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-#     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq)
+echo $(aws ec2 run-instances --image-id $AMI_ID \
+                --instance-type t3.micro \
+                --security-group-ids ${SGID} \
+                --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+                --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq)
+echo $(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --count 1 \
+    --instance-type t2.micro \
+    --security-group-ids $SECURITY_GROUPID \
+    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq)
     
     
     #--key-name payment \
