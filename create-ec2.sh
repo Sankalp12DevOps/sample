f33a564b834435a38fa1fa51e1cf39e8054689af
Sankalp12DevOps <sankalp12m@gmail.com>
2023-03-17 17:56:31 +0530

    hello

--- create-ec2.sh
+++ create-ec2.sh
@@ -18,4 +18,4 @@ PRIVATEIP=$(aws ec2 run-instances \
 
 sed -e "s/COMPONENT/$COMPONENT/" -e "s/IPADDRESS/$PRIVATEIP/" components/record.json > /tmp/r53.json
 
-aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch /tmp/r53.json
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
